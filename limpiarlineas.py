23b35e05b746b52d4a4f898bfc7392027657e640
UCIX0 <javier.uc@outlook.com>
2024-01-08 02:39:03 -0600

    flask install

--- limpiarlineas.py
+++ limpiarlineas.py
@@ -5,6 +5,11 @@ import os
 sys_mess = r"(\d{1,2}/\d{1,2}/\d{4}),\s(\d{1,2}:\d{2}\s[ap]\.\s?m\.)\s-\s(?!.*:)"
 # Define la expresión regular de mensajes del sistema en formato 24hrs
 sys_mess2 = r"(\d{1,2}/\d{1,2}/\d{4}),\s((?:[01]?\d|2[0-3]):[0-5]\d)\s-\s(?!.*:)"
+# Detecta el patron de un mensaje en formato 12hrs
+pattern = r"(\d{1,2}/\d{1,2}/\d{4}),\s(\d{1,2}:\d{2}\s[ap]\.\s?m\.)\s-\s(\+?\d*\s?\(?\d{3}\)?\s?\d{3}-\d{4}|[^:]+):\s(.*)"
+# Detecta el patron de un mensaje en formato 24hrs
+pattern2 = r"(\d{1,2}/\d{1,2}/\d{4}),\s((?:[01]?\d|2[0-3]):[0-5]\d)\s-\s(\+?\d*\s?\(?\d{3}\)?\s?\d{3}-\d{4}|[^:]+):\s(.*)"
+
 
 def filtrar_y_escribir(archivo):
 	archivo_temporal = "temporalfilepy"
@@ -46,7 +51,28 @@ def filtrar_y_escribir(archivo):
 		except Exception as e:
 			print(f"No se pudo eliminar el archivo temporal: {e}")
 
+
+def calcular_porcentaje(archivo):
+	"""
+	cuenta el numero de lineas que sigue el patron dado, y devuelve el porcentaje que sigue este patron
+	"""
+	total_lineas = 0
+	lineas_validas = 0
+
+	with open(archivo, 'r', encoding='utf-8') as file:
+		for line in file:
+			total_lineas += 1
+			if re.match(pattern, line) or re.match(pattern2, line):
+				lineas_validas += 1
+	if total_lineas == 0:
+		return 0
+	# Calcular el porcentaje de líneas que siguen la estructura
+	porcentaje = (lineas_validas / total_lineas) * 100
+	return porcentaje
+
+
 # Nombre del archivo a procesar
 archivo = "chatix.txt"
-
+porcentaje = calcular_porcentaje(archivo)
+print(f'El {porcentaje:.2f}% de las líneas sigue la estructura especificada.')
 filtrar_y_escribir(archivo)
